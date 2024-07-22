1cdf1c99f11f44baba8168bb5d8b2f15f3dedb2a
leoncico <=>
2024-07-21 20:53:59 -0500

    Pruebas con el front

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 
-let mazo = []
+let mazo = [];
 
 let colores = {
     trebol:"negro",
     diamante:"rojo",
     pica:"negro",
     corazon:"rojo"
-}
+};
 
-let contImg = 1
+let contImg = 1;
 
 function crearBaraja(){
     for(let valor in colores){
@@ -17,14 +17,29 @@ function crearBaraja(){
                 numero: j,
                 simbolo: valor,
                 color: colores[valor],
-                imagen: "Bitmap" + contImg
-            }
-            contImg += 1
-            mazo.push(carta)
-        }
-    }
-}
+                imagen: "img/Bitmap" + contImg + ".png"
+            };
+            contImg += 1;
+            mazo.push(carta);
+        };
+    };
+};
 
-crearBaraja()
+function iniciarBaraja(){
+    let pila = [];
+    let cantidadCartas = 1;
 
-console.log(mazo)
+    
+    for(let i=0; i<52; i++){
+        let carta = $('<img>');
+        carta.attr("src", mazo[i].imagen);
+        $('#tablero').append(carta);
+    };
+};
+
+crearBaraja();
+iniciarBaraja();
+
+
+
+console.log(mazo);
