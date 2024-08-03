450a70e886ccb7266ca256e1edee7d69f55f6365
leoncico <=>
2024-08-03 16:37:13 -0500

    Cambio de estructura xd

---
+++ js/Carta.js
@@ -0,0 +1,19 @@
+class Carta {
+    constructor(numero, simbolo, color, imagen, visible){
+        this.numero = numero;
+        this.simbolo = simbolo;
+        this.color = color;
+        this.imagen = imagen;
+        this.visible = visible;
+    }
+
+    isVisible() {
+        return this.visible;
+    }
+
+    setVisible(visible){
+        this.visible = visible;
+    }
+
+}
+
--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 
 let mazo = [];
+let pilas = [];
 
 let colores = {
     trebol:"negro",
@@ -25,21 +26,42 @@ function crearBaraja(){
     };
 };
 
+function mezclarCartas(){
+    for (let i = mazo.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [mazo[i], mazo[j]] = [mazo[j], mazo[i]];
+    }
+    return mazo;
+}
+
 function iniciarBaraja(){
-    let pila = [];
+
     let cantidadCartas = 1;
 
-    
-    for(let i=0; i<52; i++){
-        let carta = $('<img>');
-        carta.attr("src", mazo[i].imagen);
-        $('#tablero').append(carta);
+    for(let i=0; i<7; i++){ // cambiar a while
+        let pila = $('<div></div>');
+        pila.attr("class", 'pila');
+        while(cantidadCartas < 0){
+            let carta = $('<img>');
+            carta.attr("src", mazo[i].imagen);
+            $('.pila').append(carta);
+        };
+        $('#tablero').append(pila);
     };
+    
 };
+
+function formarPila(cantidad){
+    let pila = [];
+    for(let i=0; i<cantidad; i++){
+        let carta = mazo.pop();
+        pila.push(carta);
+    };
+    
+}
 
 crearBaraja();
 iniciarBaraja();
-
-
+mezclarCartas();
 
 console.log(mazo);
