a2f64d3df93e43bce254ce89fb6dc7d7cf4f1029
leoncico <=>
2024-08-21 15:36:20 -0500

    Mejora GUI

--- inicio.js
+++ inicio.js
@@ -1,11 +1,6 @@
 document.getElementById('inicio-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    
     const nombreJugador = document.getElementById('nombre-jugador').value;
-    
-    // Guardar el nombre del jugador en el almacenamiento local
     localStorage.setItem('nombreJugador', nombreJugador);
-    
-    // Redireccionar a la página del juego
     window.location.href = 'index.html'; 
 });
